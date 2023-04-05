9e0cf73780b67eece4e259e3819b0137132d6d4f
Jonata Silva <jonatasilva118@gmail.com>
2023-04-04 21:50:02 -0300

    Fix: Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,35 +4,31 @@ import Men from "../assets/img-to-home.png";
 import { Header } from "./Header";
 import { Faixa } from "./Faixa";
 
-export const Home = () => {
-  return (
+
+export const Home = (props) => {
+  return (    
     <>
       <Header />
-      <main className="bg-background pt-12 flex w-full gap-5 items-center justify-around">
+      <main className="bg-background pt-12 px-2.5 flex w-full gap-5 items-center justify-around flex-wrap">
         <section>
           <img src={Ui} aria-hidden="true" />
         </section>
         <section>
           <img src={Men} aria-hidden="true" />
         </section>
-        <section>
-          <div>
-            <p><span>+</span>150</p><p><span>HAPPY</span> STUDENTS</p>
-          </div>
-          <div>
-            <p><span>+</span>50</p><p><span>CERTIFIED</span> COURSES</p>
-          </div>
-          <div>
-            <p><span>+</span>1000</p>
-            <p><span>AWARDS</span> RECEIVED</p>
-            <div className="flex gap-5">
-            <button className="text-white bg-violeta">LETS START</button>
-            <button className="text-white">READ MORE</button>
-            </div>
+        <section className="flex flex-col w-72 justify-center">
+          <h1 className="text-white text-xl font-semibold">START GUIDE FOR BEGINNER DESIGNERS</h1>
+          <p className="text-white">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam soluta
+            esse harum facere qui alias, excepturi.
+          </p>
+          <div className="flex gap-5 justify-center">
+            <button className="text-white text-xs bg-violeta p-2">LETS START</button>
+            <button className="text-white text-xs p-1">READ MORE</button>
           </div>
         </section>
       </main>
-      <Faixa/>
+      <Faixa />
     </>
   );
 };

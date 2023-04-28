80aa39deb7829c4f6b6b08008b4b6fc59f6df5f5
Jonata Silva <jonatasilva118@gmail.com>
2023-04-28 20:50:16 -0300

    Add card

---
+++ src/components/Button.jsx
@@ -0,0 +1,3 @@
+export function ButtonViolet({ name }) {
+  return <button className="text-white text-xs bg-violeta p-2">{name}</button>;
+}
--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,12 +1,21 @@
 import edition from "../assets/Edition.png";
+import { ButtonViolet } from "./Button";
 
 function CourseCards({ number }) {
   return (
-    <div className="flex flex-col gap-4 w-10">
+    <div className="flex flex-col gap-4 p-3 w-80 h-96 border-4 border-white items-center">
       <div className="flex gap-3">
         <p className="text-white text-5xl font-extrabold">COURSE</p>
         <span className="text-violeta text-5xl font-extrabold">{number}</span>
       </div>
+      <div>
+        <p className="text-violeta">_______</p>
+      </div>
+      <p className="text-white">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam soluta
+        esse harum facere qui alias, excepturi.
+      </p>
+      <ButtonViolet name={"READ MORE"} />
     </div>
   );
 }
@@ -23,6 +32,7 @@ export function Courses() {
         </p>
         <img src={edition} aria-hidden="true" />
       </div>
+
       <div className="flex gap-3 flex-wrap"></div>
       <CourseCards number={1} />
     </div>

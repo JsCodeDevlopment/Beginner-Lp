6d47245dd99367ae64b3cf702028aa6847f27d3a
Jonata Silva <jonatasilva118@gmail.com>
2023-04-28 21:15:21 -0300

    fix card

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -3,7 +3,7 @@ import { ButtonViolet } from "./Button";
 
 function CourseCards({ number }) {
   return (
-    <div className="flex flex-col gap-4 p-3 w-80 h-96 border-4 border-white items-center">
+    <div className="flex flex-col gap-4 p-3 w-64 h-80 border-4 border-white justify-center items-center">
       <div className="flex gap-3">
         <p className="text-white text-5xl font-extrabold">COURSE</p>
         <span className="text-violeta text-5xl font-extrabold">{number}</span>
@@ -22,7 +22,7 @@ function CourseCards({ number }) {
 
 export function Courses() {
   return (
-    <div className="flex w-full h-80 flex-col bg-background">
+    <div className="flex w-full h-vh flex-col bg-background">
       <div className="flex p-10 gap-2 justify-between">
         <p className="text-white text-5xl font-extrabold">
           FIND{" "}
@@ -33,8 +33,13 @@ export function Courses() {
         <img src={edition} aria-hidden="true" />
       </div>
 
-      <div className="flex gap-3 flex-wrap"></div>
+      <div className="flex flex-wrap gap-3 p-3 justify-evenly">
+        
       <CourseCards number={1} />
+      <CourseCards number={2} />
+      <CourseCards number={3} />
+      <CourseCards number={4} />
+      </div>
     </div>
   );
 }

1ed03f6c0d1401f8f6656cfd3169a5baee180f3f
Jonata Silva <jonatasilva118@gmail.com>
2023-09-19 16:04:54 -0300

    Finally Finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,55 @@
 import "../index.css";
 import Logo from "../assets/Your-Logo.png";
 import Menu from "../assets/menu.png";
+import CloseMenu from "../assets/close-menu.png";
 import { RightMenu } from "./Menu";
+import { useState } from "react";
+
+const links = [
+  {
+    text: "HOME",
+    href: "#Home",
+  },
+  {
+    text: "COURSE",
+    href: "#Courses",
+  },
+  {
+    text: "ABOUT",
+    href: "#About",
+  },
+  {
+    text: "BLOG",
+    href: "#Newsletter",
+  },
+];
 
 export const Header = () => {
+  const [menuVisible, setmenuVisible] = useState(false);
+
   return (
     <div className="w-full z-10 h-12 bg-background fixed">
       <div className="flex items-center text-white text-xl p-3 justify-center gap-32">
         <a href="#Home">
           <img src={Logo} aria-hidden="true" className="w-30 h-5" />
         </a>
-        <button className="inline-flex w-5 h-5 rounded ml-auto lg:hidden">
-          <img src={Menu} aria-hidden="true" />
-        </button>
-        <a href="#Home" className="max-lg:hidden">HOME</a>
-        <a href="#Courses" className="max-lg:hidden">COURSES</a>
-        <a href="#About" className="max-lg:hidden">ABOUT</a>
-        <a href="#Newsletter" className="max-lg:hidden">BLOG</a>
+        <img
+          className="inline-flex w-5 h-5 rounded ml-auto lg:hidden"
+          src={menuVisible ? CloseMenu : Menu}
+          aria-hidden="true"
+          onClick={() => setmenuVisible(!menuVisible)}
+        />
+
+        {links.map((link) => (
+          <li
+            key={link.text}
+            className="max-lg:hidden list-none relative hover:before:absolute hover:before:bg-violeta hover:before:w-20 hover:before:h-1 hover:before:top-6"
+          >
+            <a href={link.href}>{link.text}</a>
+          </li>
+        ))}
       </div>
-      <>
-        <RightMenu/>
-      </>
+      <>{menuVisible ? <RightMenu /> : <></>}</>
     </div>
   );
 };
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,28 +2,28 @@ import React from "react";
 const links = [
   {
     text: "HOME",
-    href: "#home",
+    href: "#Home",
   },
   {
     text: "COURSE",
-    href: "#course",
+    href: "#Courses",
   },
   {
     text: "ABOUT",
-    href: "#about",
+    href: "#About",
   },
   {
     text: "BLOG",
-    href: "#blog",
+    href: "#Newsletter",
   },
 ];
 
 export function RightMenu() {
   return (
     <div className="relative bottom-2 block text-white text-xl ">
-      <ul className="flex flex-col justify-evenly pl-2 bg-background w-80 h-96 z-50 relative top-0 left-3/4">
+      <ul className="flex flex-col justify-evenly pl-2 bg-background w-96 h-96 z-50 relative top-0 left-3/4 max-[500px]:left-80 max-[434px]:left-40">
         {links.map((link) => (
-          <li key={link.text} className="relative hover:before:absolute hover:before:bg-violeta hover:before:w-32 hover:before:h-2 hover:before:top-6">
+          <li key={link.text} className="relative hover:before:absolute hover:before:bg-violeta hover:before:w-32 hover:before:h-1 hover:before:top-6">
             <a href={link.href}>{link.text}</a>
           </li>
         ))}
